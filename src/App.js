a77acc1a6500b53956a0af1daf3675e3ca9ed4ef
Habeebope <habeebullahokunlola@gmail.com>
2023-11-08 17:16:50 +0100

    General change, no specification

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import React from "react";
 import "./index.css";
 import { Routes, Route } from "react-router-dom";
-import Section from "./Section";
-import Nav from "./Nav";
-import Item from "./Item";
-import Subscribe from "./Subscribe";
-import Footer from "./Footer";
-import About from "./About";
-import Cssdesign from "./Cssdesign";
-import Projectcss from "./Projectcss";
-import Projectvanilla from "./Projectvanilla";
-import Projectgame from "./Projectgame";
-import Projectlanding from "./Projectlanding";
-import Projectreact from "./Projectreact";
-import Header from "./Header";
+import Home from "./main/Home";
+import Item from "./submain/Item";
+import Subscribe from "./main/Subscribe";
+import Footer from "./main/Footer";
+import About from "./main/About";
+import Cssdesign from "./submain/Cssdesign";
+import Projectcss from "./submain/Projectcss";
+import Projectvanilla from "./submain/Projectvanilla";
+import Projectgame from "./submain/Projectgame";
+import Projectlanding from "./submain/Projectlanding";
+import Projectreact from "./submain/Projectreact";
+import Header from "./main/Header";
+
 
 export default function App() {
+
   return (
     <div className="index-class">
-      <div className="home">
-        <Nav />
         <Routes>
-          <Route path="/" element={<Section />} />
+          <Route path="/" element={<Home />} />
           <Route path="/cssdesign" element={<Projectcss />} />
           <Route path="/vanilla" element={<Projectvanilla />} />
           <Route path="/landing" element={<Projectlanding />} />
           <Route path="/game" element={<Projectgame />} />
           <Route path="/react" element={<Projectreact />} />
         </Routes>
-      </div>
       <Header />
       <Routes>
         <Route path="/" element={<Item />} />
--- src/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header() {
-  return (
-    <div className="heading">
-      <h1>Project Features</h1>
-    </div>
-  );
-}
